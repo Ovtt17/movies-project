61fa1c3a73a4a60c81aab210173b6753f70c4488
Ovett Mora <ovettmora17@gmail.com>
2024-10-14 15:10:35 -0600

    Add FavoriteMovies component and AppRoutes for routing

---
+++ src/components/movie/FavoriteMovies.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface FavoriteMoviesProps {
+  
+}
+
+const FavoriteMovies: FC<FavoriteMoviesProps> = ({  }) => {
+  return (
+    <div>
+      FavoriteMovies
+    </div>
+  );
+}
+
+export default FavoriteMovies;
