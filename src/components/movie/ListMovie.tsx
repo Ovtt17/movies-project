837202c2608e75cdc465daa338d2b1bd7a9509a3
Ovett Mora <ovettmora17@gmail.com>
2024-10-13 22:24:31 -0600

    Refactor ListMovie component to display a grid of MovieCard components

--- src/components/movie/ListMovie.tsx
+++ src/components/movie/ListMovie.tsx
@@ -1,8 +1,28 @@
+import movies from '../../data/movies';
+import MovieCard from './MovieCard';
+
 const ListMovie = () => {
-  
+  const handleViewMore = () => {
+    alert('Ver más');
+  }
+
+  const movieList = movies.map((movie) => (
+    <MovieCard
+      key={movie.imdb.id}
+      title={movie.title}
+      plot={movie.plot}
+      released={movie.releaseInfo.released}
+      poster={movie.metadata.poster}
+      onViewMore={handleViewMore}
+    />
+  ));
+
   return (
-    <div>
-      ListMovie
+    <div className='container mx-auto p-4'>
+      <h2 className='text-2xl font-bold mb-4 text-center'>Lista de Películas</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {movieList}
+      </div>
     </div>
   );
 }
