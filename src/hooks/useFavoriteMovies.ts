43fd99b36980a6fd6c057749f97c1b65169bc60e
Ovett Mora <ovettmora17@gmail.com>
2024-10-14 15:17:05 -0600

    Add useFavoriteMovies hook for managing favorite movies

---
+++ src/hooks/useFavoriteMovies.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+interface Movie {
+  id: number;
+  title: string;
+  year: number;
+}
+
+const useFavoriteMovies = () => {
+  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const storedFavorites = localStorage.getItem('favoriteMovies');
+    if (storedFavorites) {
+      setFavoriteMovies(JSON.parse(storedFavorites));
+    }
+  }, []);
+
+  return favoriteMovies;
+};
+
+export default useFavoriteMovies;
