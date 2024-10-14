1f6f741fd2f0b32896d038ce625f3c8ffe9f6a23
Ovett Mora <ovettmora17@gmail.com>
2024-10-13 22:24:08 -0600

    Add MovieCard component

---
+++ src/components/movie/MovieCard.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+
+interface MovieCardProps {
+  title: string;
+  plot: string;
+  released: string;
+  poster: string;
+  onViewMore: () => void;
+}
+
+const MovieCard: FC<MovieCardProps> = ({
+  title,
+  plot,
+  released,
+  poster,
+  onViewMore,
+}) => {
+  return (
+    <div className='cursor-pointer bg-[#252525] rounded-lg shadow-md overflow-hidden transform transition-transform hover:scale-105 duration-200'>
+      <div className='relative'>
+        <img
+          src={poster}
+          alt={title}
+          className='w-full h-64 object-cover hover:opacity-75'
+        />
+        <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-200'>
+          <button onClick={onViewMore} className='text-white text-lg font-bold px-4 py-2 bg-black bg-opacity-75 rounded-md'>
+            Ver más
+          </button>
+        </div>
+      </div>
+      <div className='p-4'>
+        <h3 className='text-lg font-semibold'>{title}</h3>
+        <p className='text-sm text-gray-100'>{released}</p>
+        <p className='text-sm text-gray-200'>{plot}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
